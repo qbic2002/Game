90ab4207c462c63ad9ab7b94c818f427291f37e6
qbic2002 <44712319+qbic2002@users.noreply.github.com>
2018-11-03 01:20:11 +0300

    Add files via upload

---
+++ PlayerMove.cs
@@ -0,0 +1,29 @@
+﻿
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+    private Rigidbody2D rb;
+    public float speed = 20f;
+    public float speedr = 2f;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.rotation = Quaternion.Euler(0, 0, transform.eulerAngles.z + speedr);
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.rotation = Quaternion.Euler(0, 0, transform.eulerAngles.z - speedr);
+            
+        }
+        //print(transform.eulerAngles.z);
+        rb.velocity = new Vector2(Mathf.Sin(Mathf.Deg2Rad * transform.eulerAngles.z) * -speed, Mathf.Cos(Mathf.Deg2Rad * transform.eulerAngles.z) * speed);
+	}
+}
