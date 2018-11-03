2279f91c53dbaea8148821e9fdeac918646cd961
qbic2002 <44712319+qbic2002@users.noreply.github.com>
2018-11-03 15:27:19 +0300

    Add files via upload

--- AsteroidSpawn.cs
+++ AsteroidSpawn.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class AsteroidSpawn : MonoBehaviour {
 
     public GameObject aster;
+    public float x;
+    public float y;
+    public int p;
+    private Rigidbody2D bullet_rb;
 
 	void Start () {
         StartCoroutine(spawn());
@@ -19,7 +23,44 @@ public class AsteroidSpawn : MonoBehaviour {
     {
         while (true)
         {
-            Instantiate(aster, new Vector2(Random.Range(-16, 16), Random.Range(-8, 8)), Quaternion.Euler(0,0,Random.Range(0,360)));
+            p = Random.Range(1, 4);
+            switch (p)
+            {
+                case 1:
+                    y = 8.9F;
+                    x = Random.Range(-16.9F, 16.9F);
+                    break;
+                case 2:
+                    y = -8.9F;
+                    x = Random.Range(-16.9F, 16.9F);
+                    break;
+                case 3:
+                    x = 16.9F;
+                    y = Random.Range(-8.9F, 8.9F);
+                    break;
+                case 4:
+                    x = -16.9F;
+                    y = Random.Range(-8.9F, 8.9F);
+                    break;
+            }
+            
+            GameObject bullet = Instantiate(aster, new Vector2(x, y), Quaternion.Euler(0,0,Random.Range(0,360)));
+            bullet_rb = bullet.GetComponent<Rigidbody2D>();
+            switch (p)
+            {
+                case 1:
+                    bullet_rb.velocity = new Vector2(Random.Range(0F, 1F), -Random.Range(0.2F, 1F));
+                    break;
+                case 2:
+                    bullet_rb.velocity = new Vector2(Random.Range(0F, 1F), Random.Range(0.2F, 1F));
+                    break;
+                case 3:
+                    bullet_rb.velocity = new Vector2(-Random.Range(0.2F, 1F), Random.Range(0F, 1F));
+                    break;
+                case 4:
+                    bullet_rb.velocity = new Vector2(Random.Range(0.2F, 1F), Random.Range(0F, 1F));
+                    break;
+            }
             yield return new WaitForSeconds(3);
         }
     }
